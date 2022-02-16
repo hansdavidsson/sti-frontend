178616e15058c339ed4b060a2216a3576e491ce4
hansdavidsson <92034296+hansdavidsson@users.noreply.github.com>
2022-02-16 13:45:49 +0100

    fotball table added

--- public/js/table.js
+++ public/js/table.js
@@ -1,10 +1,45 @@
 function createCol() {
+    var seq=0;
+
+function myFunction(){
+// Increment the value
+seq += 1
+
+// The string value that will be displayed
+var value = '';
+
+// If sequence is less than 10, prefix with 000
+if(seq < 10) {
+    value = '000' + seq;
+}
+// If sequence is less than 100, prefix with 00
+else if(seq < 100) {
+    value = '00' + seq;
+}
+// If sequence is less than 1000, prefix with 0
+else if(seq < 1000) {
+    value = '0' + seq;
+}
+// Otherwise, just use the value directly
+else {
+    value = seq;
+}
+
+// Display the formatted value (ie prefixed with 0's)
+document.getElementById("demo").value = value;
+
+}
+
+
+
+
+
     var x = document.createElement("TABLE");
     x.setAttribute("id", "myTable");
     document.body.appendChild(x);
   
     var y = document.createElement("TR");
-    y.setAttribute("id", "myTr");
+    y.setAttribute("id", seq);
     document.getElementById("myTable").appendChild(y);
   
     var z = document.createElement("TD");
